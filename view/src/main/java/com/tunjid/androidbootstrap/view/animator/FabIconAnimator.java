cd74fd53027614379e1153be27827b51767d049d
Adetunji Dahunsi <tj@MacBook-Air-2.lan>
2018-10-23 22:21:01 -0400

    Wip of animators

---
+++ view/src/main/java/com/tunjid/androidbootstrap/view/animator/FabIconAnimator.java
@@ -0,0 +1,156 @@
+package com.tunjid.androidbootstrap.view.animator;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.drawable.Drawable;
+import android.transition.AutoTransition;
+import android.transition.Transition;
+import android.transition.Transition.TransitionListener;
+import android.transition.TransitionManager;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import com.google.android.material.button.MaterialButton;
+import com.tunjid.androidbootstrap.view.R;
+
+import java.util.Objects;
+
+public class FabIconAnimator {
+
+    private static final int DURATION = 200;
+    private static final float TWITCH_END = 20.0f;
+    private static final float TWITCH_START = 0.0f;
+    private static final String ROTATION_Y_PROPERTY = "rotationY";
+
+    private int fabSize;
+    private boolean isAnimating;
+
+    private State state;
+    private final MaterialButton button;
+    private final ConstraintLayout container;
+
+    private final TransitionListener listener =  new TransitionListener() {
+        public void onTransitionStart(Transition transition) { isAnimating = true; }
+
+        public void onTransitionEnd(Transition transition) { isAnimating = false; }
+
+        public void onTransitionCancel(Transition transition) {isAnimating = false; }
+
+        public void onTransitionPause(Transition transition) { }
+
+        public void onTransitionResume(Transition transition) {}
+    };
+
+    public FabIconAnimator(ConstraintLayout container) {
+        this.container = container;
+        this.button = (MaterialButton) container.getChildAt(0);
+        this.fabSize = container.getResources().getDimensionPixelSize(R.dimen.fab_size);
+    }
+
+    public static State newState(CharSequence text, Drawable icon) { return new SimpleState(text, icon);}
+
+    public void update(@NonNull State state) {
+        boolean isSame = state.equals(this.state);
+        this.state = state;
+        animateChange(state, isSame);
+    }
+
+    public void setExtended(boolean extended) { setExtended(extended, false); }
+
+    private boolean isExtended() {
+        return button.getLayoutParams().height != this.button.getResources().getDimensionPixelSize(R.dimen.fab_size);
+    }
+
+    private void animateChange(State state, boolean isSame) {
+        boolean extended = isExtended();
+        this.button.setText(state.getText());
+        this.button.setIcon(state.getIcon());
+        setExtended(extended, !isSame);
+        if (!extended) twitch();
+    }
+
+    private void setExtended(boolean extended, boolean force) {
+        if (this.isAnimating || (extended && isExtended() && !force)) return;
+
+        ConstraintSet set = extended ? expanded() : collapsed();
+        TransitionManager.beginDelayedTransition(this.container, new AutoTransition().addListener(listener).setDuration(150));
+        if (extended) this.button.setText(this.state.getText());
+        else this.button.setText("");
+        set.applyTo(this.container);
+
+    }
+
+    private void twitch() {
+        AnimatorSet set = new AnimatorSet();
+        set.play(animateProperty(TWITCH_END, TWITCH_START)).after(animateProperty(TWITCH_START, TWITCH_END));
+        set.start();
+    }
+
+    private ConstraintSet expanded() {
+        ConstraintSet set = fabSet();
+        set.constrainHeight(this.button.getId(), ConstraintSet.WRAP_CONTENT);
+        set.constrainWidth(this.button.getId(), -ConstraintSet.WRAP_CONTENT);
+        return set;
+    }
+
+    private ConstraintSet collapsed() {
+        ConstraintSet set = fabSet();
+        set.constrainHeight(this.button.getId(), this.fabSize);
+        set.constrainWidth(this.button.getId(), this.fabSize);
+        return set;
+    }
+
+    private ConstraintSet fabSet() {
+        int buttonId = this.button.getId();
+        int containerId = this.container.getId();
+        ConstraintSet set = new ConstraintSet();
+        set.connect(buttonId, 3, containerId, 3);
+        set.connect(buttonId, 7, containerId, 7);
+        set.connect(buttonId, 6, containerId, 6);
+        set.connect(buttonId, 4, containerId, 4);
+        return set;
+    }
+
+    @NonNull
+    private ObjectAnimator animateProperty(float start, float end) {
+        return ObjectAnimator.ofFloat(container, ROTATION_Y_PROPERTY, start, end).setDuration(DURATION);
+    }
+
+    public static abstract class State {
+
+        public abstract Drawable getIcon();
+
+        public abstract CharSequence getText();
+    }
+
+    public static class SimpleState extends State {
+
+        public Drawable icon;
+        public CharSequence text;
+
+        private SimpleState(CharSequence text, Drawable icon) {
+            this.text = text;
+            this.icon = icon;
+        }
+
+        public CharSequence getText() { return text; }
+
+        public Drawable getIcon() { return icon; }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            SimpleState that = (SimpleState) o;
+            return Objects.equals(icon, that.icon) &&
+                    Objects.equals(text, that.text);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(icon, text);
+        }
+    }
+}
---
+++ view/src/main/java/com/tunjid/androidbootstrap/view/animator/ViewPagerIndicatorAnimator.java
@@ -0,0 +1,240 @@
+package com.tunjid.androidbootstrap.view.animator;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.widget.ImageView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager.widget.ViewPager.OnAdapterChangeListener;
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener;
+
+import static androidx.core.content.ContextCompat.getDrawable;
+
+public class ViewPagerIndicatorAnimator {
+
+    private static final int MIN_INDICATORS_TO_SHOW = 2;
+    private static final int MIN_VIEWS_IN_LAYOUT = 3;
+
+    private int indicatorCount;
+    private int guideLineWidth;
+
+    private final int indicatorHeight;
+    private final int indicatorPadding;
+    private final int indicatorWidth;
+
+    @DrawableRes
+    private final int inActiveDrawable;
+
+    private final ConstraintLayout container;
+    private final ViewPager viewPager;
+    private final ImageView indicator;
+    private final View guide;
+
+    private Implementation implementation;
+
+    private ViewPagerIndicatorAnimator(int indicatorWidth, int indicatorHeight, int indicatorPadding,
+                                       @DrawableRes int activeDrawable, @DrawableRes int inActiveDrawable, @DrawableRes int backgroundDrawable,
+                                       ConstraintLayout container, ViewPager viewPager, View guide) {
+
+        this.indicatorWidth = indicatorWidth;
+        this.indicatorHeight = indicatorHeight;
+        this.inActiveDrawable = inActiveDrawable;
+        this.indicatorPadding = indicatorPadding;
+        this.container = container;
+        this.viewPager = viewPager;
+        this.guide = guide;
+        this.indicator = buildIndicator();
+        this.implementation = new Implementation();
+        PagerAdapter adapter = viewPager.getAdapter();
+
+        if (adapter != null) buildIndicators(adapter);
+        Context context = viewPager.getContext();
+
+        if (backgroundDrawable != 0) guide.setBackground(getDrawable(context, backgroundDrawable));
+        if (activeDrawable != 0) this.indicator.setImageResource(activeDrawable);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private ImageView buildIndicator() {
+        ImageView result = new ImageView(this.container.getContext());
+        result.setId(View.generateViewId());
+
+        LayoutParams params = new LayoutParams(indicatorWidth, indicatorHeight);
+        params.rightMargin = params.leftMargin = indicatorPadding;
+        params.bottomToBottom = guide.getId();
+
+        container.addView(result, params);
+        return result;
+    }
+
+    private void buildIndicators(@Nullable PagerAdapter newAdapter) {
+        if (newAdapter == null) return;
+
+        int pageCount = newAdapter.getCount();
+
+        if (this.container.getChildCount() > MIN_VIEWS_IN_LAYOUT) {
+            this.container.removeViews(MIN_VIEWS_IN_LAYOUT, this.indicatorCount);
+        }
+        if (pageCount < MIN_INDICATORS_TO_SHOW) {
+            this.indicator.setVisibility(View.GONE);
+            return;
+        }
+        this.indicator.setVisibility(View.VISIBLE);
+        int[] chainIds = new int[pageCount];
+        this.indicatorCount = 0;
+        while (this.indicatorCount < pageCount) {
+            ImageView imageView = buildIndicator();
+            chainIds[this.indicatorCount] = imageView.getId();
+            imageView.setImageResource(this.inActiveDrawable);
+            this.indicatorCount++;
+        }
+
+        int guideId = this.guide.getId();
+        int indicatorId = this.indicator.getId();
+
+        this.guide.getLayoutParams().width = pageCount * (this.indicatorWidth + (this.indicatorPadding * 2));
+
+        ConstraintSet set = new ConstraintSet();
+        set.clone(container);
+        set.createHorizontalChain(guideId, ConstraintSet.LEFT, guideId, ConstraintSet.RIGHT, chainIds, null, 2);
+        set.connect(indicatorId, ConstraintSet.LEFT, guideId, ConstraintSet.LEFT);
+        set.connect(chainIds[0], ConstraintSet.LEFT, guideId, ConstraintSet.LEFT);
+        set.connect(chainIds[pageCount - 1], ConstraintSet.RIGHT, guideId, ConstraintSet.RIGHT);
+        set.applyTo(container);
+
+        ViewTreeObserver observer = this.guide.getViewTreeObserver();
+        if (observer.isAlive()) {
+            observer.addOnGlobalLayoutListener(this.implementation);
+            guide.invalidate();
+        }
+    }
+
+    public class Implementation extends DataSetObserver implements OnPageChangeListener, OnAdapterChangeListener, OnGlobalLayoutListener {
+
+        private float lastPositionOffset;
+
+        private Implementation() {
+            PagerAdapter adapter = viewPager.getAdapter();
+            if (adapter != null) adapter.registerDataSetObserver(this);
+
+            viewPager.addOnPageChangeListener(this);
+            viewPager.addOnAdapterChangeListener(this);
+        }
+
+        public void onAdapterChanged(@NonNull ViewPager viewPager, @Nullable PagerAdapter oldAdapter, @Nullable PagerAdapter newAdapter) {
+            if (newAdapter == null) return;
+            newAdapter.registerDataSetObserver(this);
+            buildIndicators(newAdapter);
+        }
+
+        public void onPageScrolled(int position, float fraction, int pixelOffset) {
+            float currentPositionOffset = ((float) position) + fraction;
+            boolean toTheRight = currentPositionOffset > lastPositionOffset;
+
+            indicator.setTranslationX(getTranslation(toTheRight, toTheRight ? position : position + 1, fraction));
+            lastPositionOffset = currentPositionOffset;
+        }
+
+        public void onGlobalLayout() {
+            ViewTreeObserver observer = guide.getViewTreeObserver();
+            if (observer.isAlive()) observer.removeOnGlobalLayoutListener(this);
+
+            int initialIndex = viewPager.getCurrentItem() + 1;
+            indicator.setTranslationX(getTranslation(false, initialIndex, 0.0f));
+            guideLineWidth = guide.getWidth();
+        }
+
+        private float getTranslation(boolean toTheRight, int originalPosition, float fraction) {
+            if (!toTheRight) fraction = 1.0f - fraction;
+
+            float chunkWidth = ((float) guideLineWidth) / ((float) viewPager.getAdapter().getCount());
+            float currentChunk = ((float) originalPosition) * chunkWidth;
+            float diff = chunkWidth * fraction;
+
+            return toTheRight ? currentChunk + diff : currentChunk - diff;
+        }
+
+        public void onPageSelected(int position) {
+        }
+
+        public void onPageScrollStateChanged(int state) {
+        }
+    }
+
+    public static class Builder {
+        private int activeDrawable;
+        private int backgroundDrawable;
+        private int inActiveDrawable;
+        private int indicatorHeight;
+        private int indicatorPadding;
+        private int indicatorWidth;
+        private ConstraintLayout container;
+        private ViewPager viewPager;
+        private View guideLine;
+
+        public Builder setIndicatorWidth(int indicatorWidth) {
+            this.indicatorWidth = indicatorWidth;
+            return this;
+        }
+
+        public Builder setIndicatorHeight(int indicatorHeight) {
+            this.indicatorHeight = indicatorHeight;
+            return this;
+        }
+
+        public Builder setIndicatorPadding(int indicatorPadding) {
+            this.indicatorPadding = indicatorPadding;
+            return this;
+        }
+
+        public Builder setActiveDrawable(int activeDrawable) {
+            this.activeDrawable = activeDrawable;
+            return this;
+        }
+
+        public Builder setInActiveDrawable(int inActiveDrawable) {
+            this.inActiveDrawable = inActiveDrawable;
+            return this;
+        }
+
+        public Builder setBackgroundDrawable(int backgroundDrawable) {
+            this.backgroundDrawable = backgroundDrawable;
+            return this;
+        }
+
+        public Builder setContainer(ConstraintLayout container) {
+            this.container = container;
+            return this;
+        }
+
+        public Builder setViewPager(ViewPager viewPager) {
+            this.viewPager = viewPager;
+            return this;
+        }
+
+        public Builder setGuideLine(View guideLine) {
+            this.guideLine = guideLine;
+            return this;
+        }
+
+        public ViewPagerIndicatorAnimator build() {
+            return new ViewPagerIndicatorAnimator(
+                    indicatorWidth, indicatorHeight, indicatorPadding,
+                    activeDrawable, inActiveDrawable, backgroundDrawable,
+                    container, viewPager, guideLine);
+        }
+    }
+}
