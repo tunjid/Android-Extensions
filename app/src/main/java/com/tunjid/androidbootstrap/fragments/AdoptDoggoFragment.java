fb4ba59d7068b143b1f152dda2144fb3a6131f51
Adetunji Dahunsi <tjdah100@gmail.com>
2018-10-31 22:02:18 -0400

    String updates

--- app/src/main/java/com/tunjid/androidbootstrap/fragments/AdoptDoggoFragment.java
+++ app/src/main/java/com/tunjid/androidbootstrap/fragments/AdoptDoggoFragment.java
@@ -7,18 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.tunjid.androidbootstrap.R;
 import com.tunjid.androidbootstrap.adapters.ImageListAdapter.ImageListAdapterListener;
 import com.tunjid.androidbootstrap.adapters.ImageListAdapter.ImageViewHolder;
 import com.tunjid.androidbootstrap.adapters.InputAdapter;
 import com.tunjid.androidbootstrap.baseclasses.AppBaseFragment;
 import com.tunjid.androidbootstrap.model.Doggo;
+import com.tunjid.androidbootstrap.view.animator.FabExtensionAnimator;
 import com.tunjid.androidbootstrap.view.util.InsetFlags;
 
 import java.util.Arrays;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -27,6 +30,8 @@ public class AdoptDoggoFragment extends AppBaseFragment
 
     private static final String ARG_DOGGO = "doggo";
     private static final InsetFlags NO_TOP = InsetFlags.create(true, false, true, true);
+
+    private Doggo doggo;
 
     public static AdoptDoggoFragment newInstance(Doggo doggo) {
         AdoptDoggoFragment fragment = new AdoptDoggoFragment();
@@ -44,8 +49,13 @@ public class AdoptDoggoFragment extends AppBaseFragment
         return super.getStableTag() + "-" + getArguments().getParcelable(ARG_DOGGO).hashCode();
     }
 
-    @Nullable
     @SuppressWarnings("ConstantConditions")
+    @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        doggo = getArguments().getParcelable(ARG_DOGGO);
+    }
+
+    @Nullable
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_adopt_doggo, container, false);
         RecyclerView recyclerView = root.findViewById(R.id.model_list);
@@ -54,7 +64,7 @@ public class AdoptDoggoFragment extends AppBaseFragment
         recyclerView.setAdapter(new InputAdapter(Arrays.asList(getResources().getStringArray(R.array.adoption_items))));
 
         ImageViewHolder viewHolder = new ImageViewHolder(root, this);
-        viewHolder.bind(getArguments().getParcelable(ARG_DOGGO));
+        viewHolder.bind(doggo);
 
         tintView(R.color.black_50, viewHolder.thumbnail, this::setColorFilter);
         tintView(R.color.black_50, viewHolder.fullSize, this::setColorFilter);
@@ -62,13 +72,21 @@ public class AdoptDoggoFragment extends AppBaseFragment
         return root;
     }
 
-    @Override public boolean showsToolBar() {
-        return false;
+    @Override public boolean showsToolBar() { return false; }
+
+    @Override public boolean showsFab() { return true; }
+
+    @Override public InsetFlags insetFlags() { return NO_TOP; }
+
+    @Override protected FabExtensionAnimator.GlyphState getFabState() {
+        return FabExtensionAnimator.newState(getString(R.string.adopt), ContextCompat.getDrawable(requireContext(), R.drawable.ic_hug_24dp));
     }
 
-    @Override public InsetFlags insetFlags() {
-        return NO_TOP;
+    @Override protected View.OnClickListener getFabClickListener() {
+        return view -> Snackbar.make(view, getString(R.string.adopted_doggo, doggo.getName()), Snackbar.LENGTH_SHORT).show();
     }
+
+    private void setColorFilter(int color, ImageView imageView) { imageView.setColorFilter(color); }
 
     private void prepareSharedElementTransition() {
         Transition baseTransition = baseTransition();
@@ -78,9 +96,5 @@ public class AdoptDoggoFragment extends AppBaseFragment
         setExitTransition(baseTransition);
         setSharedElementEnterTransition(baseSharedTransition);
         setSharedElementReturnTransition(baseSharedTransition);
-    }
-
-    private void setColorFilter(int color, ImageView imageView) {
-        imageView.setColorFilter(color);
     }
 }
