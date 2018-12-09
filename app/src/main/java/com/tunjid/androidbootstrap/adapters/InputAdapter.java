e04ec534bdca2b44823d1ebd9503aafc33a002c5
Adetunji Dahunsi <tjdah100@gmail.com>
2018-12-09 09:59:17 -0500

    Flattened Input fields with constraint layout

--- app/src/main/java/com/tunjid/androidbootstrap/adapters/InputAdapter.java
+++ app/src/main/java/com/tunjid/androidbootstrap/adapters/InputAdapter.java
@@ -1,9 +1,16 @@
 package com.tunjid.androidbootstrap.adapters;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.material.textfield.TextInputLayout;
 import com.tunjid.androidbootstrap.R;
 import com.tunjid.androidbootstrap.view.recyclerview.InteractiveAdapter;
 import com.tunjid.androidbootstrap.view.recyclerview.InteractiveViewHolder;
@@ -11,6 +18,10 @@ import com.tunjid.androidbootstrap.view.recyclerview.InteractiveViewHolder;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import static android.text.TextUtils.isEmpty;
+import static com.tunjid.androidbootstrap.view.util.ViewUtil.listenForLayout;
 
 public class InputAdapter extends InteractiveAdapter<InputAdapter.InputViewHolder, InteractiveAdapter.AdapterListener> {
 
@@ -32,6 +43,15 @@ public class InputAdapter extends InteractiveAdapter<InputAdapter.InputViewHolde
         holder.bind(hints.get(position));
     }
 
+    @Override public void onViewDetachedFromWindow(@NonNull InputViewHolder holder) {
+        holder.text.removeTextChangedListener(holder);
+    }
+
+    @Override
+    public void onViewRecycled(@NonNull InputViewHolder holder) {
+        holder.text.removeTextChangedListener(holder);
+    }
+
     @Override
     public int getItemCount() {
         return hints.size();
@@ -43,17 +63,89 @@ public class InputAdapter extends InteractiveAdapter<InputAdapter.InputViewHolde
     }
 
 
-    static class InputViewHolder extends InteractiveViewHolder {
+    static class InputViewHolder extends InteractiveViewHolder
+            implements
+            TextWatcher {
 
-        private final TextInputLayout input;
+        private static final int HINT_ANIMATION_DURATION = 200;
+        private static final float HINT_SHRINK_SCALE = 0.8F;
+        private static final float HALF = 0.5F;
+
+        private int lastLineCount = 1;
+
+        protected final TextView hint;
+        protected final EditText text;
 
         InputViewHolder(View itemView) {
             super(itemView);
-            input = (TextInputLayout) itemView;
+            hint = itemView.findViewById(R.id.hint);
+            text = itemView.findViewById(R.id.input);
+            text.setOnFocusChangeListener((v, hasFocus) -> {
+                tintHint(hasFocus);
+                scaleHint(!hasFocus && isEmpty(text.getText()));
+            });
         }
 
-        void bind(String hint) {
-            input.setHint(hint);
+        private float getHintLateralTranslation() {
+            int width = hint.getWidth();
+            return -((width - (HINT_SHRINK_SCALE * width)) * HALF);
+        }
+
+        private float getHintLongitudinalTranslation() {
+            return -((itemView.getHeight() - hint.getHeight()) * HALF);
+        }
+
+         void bind(String hintValue) {
+            hint.setText(hintValue);
+            text.addTextChangedListener(this);
+            setTintAlpha(text.hasFocus());
+            listenForLayout(hint, () -> scaleHint(isEmpty(text.getText())));
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {/* Nothing */}
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {/* Nothing */}
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            int currentLineCount = text.getLineCount();
+            if (lastLineCount != currentLineCount) listenForLayout(hint, () -> scaleHint(false));
+            lastLineCount = currentLineCount;
+        }
+
+        private void scaleHint(boolean grow) {
+            float scale = grow ? 1F : HINT_SHRINK_SCALE;
+            float translationX = grow ? 0 : getHintLateralTranslation();
+            float translationY = grow ? 0 : getHintLongitudinalTranslation();
+
+            hint.animate()
+                    .scaleX(scale)
+                    .scaleY(scale)
+                    .translationX(translationX)
+                    .translationY(translationY)
+                    .setDuration(HINT_ANIMATION_DURATION)
+                    .start();
+        }
+
+        private void tintHint(boolean hasFocus) {
+            int start = hint.getCurrentTextColor();
+            int end = ContextCompat.getColor(hint.getContext(), hasFocus
+                    ? R.color.colorAccent
+                    : R.color.dark_grey);
+
+            ValueAnimator animator = ValueAnimator.ofObject(new ArgbEvaluator(), start, end);
+            animator.setDuration(HINT_ANIMATION_DURATION);
+            animator.addUpdateListener(animation -> hint.setTextColor((int) animation.getAnimatedValue()));
+            animator.addListener(new AnimatorListenerAdapter() {
+                public void onAnimationEnd(Animator animation) { setTintAlpha(hasFocus); }
+            });
+            animator.start();
+        }
+
+        private void setTintAlpha(boolean hasFocus) {
+            hint.setAlpha(!hasFocus ? 0.38F : 1F);
         }
     }
 }
--- view/src/main/java/com/tunjid/androidbootstrap/view/util/ViewUtil.java
+++ view/src/main/java/com/tunjid/androidbootstrap/view/util/ViewUtil.java
@@ -3,6 +3,7 @@ package com.tunjid.androidbootstrap.view.util;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 
 import androidx.annotation.LayoutRes;
 
@@ -18,5 +19,18 @@ public class ViewUtil {
 
     public static String transitionName(Object object, View view) {
         return object.hashCode() + "-" + view.getId();
+    }
+
+    public static void listenForLayout(View view, Runnable onLayout) {
+        ViewTreeObserver observer = view.getViewTreeObserver();
+        if (!observer.isAlive()) return;
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override public void onGlobalLayout() {
+                if (observer.isAlive()) observer.removeOnGlobalLayoutListener(this);
+                ViewTreeObserver current = view.getViewTreeObserver();
+                if (current.isAlive()) current.removeOnGlobalLayoutListener(this);
+                onLayout.run();
+            }
+        });
     }
 }
