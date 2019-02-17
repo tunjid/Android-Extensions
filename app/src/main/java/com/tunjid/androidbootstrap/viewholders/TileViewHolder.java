e0b0840d0059a11c50b49c2ada58d443aa2f2a5f
Adetunji Dahunsi <tjdah100@gmail.com>
2019-02-17 17:20:40 -0500

    TileViewHolder improvements, theming of toolbar text

--- app/src/main/java/com/tunjid/androidbootstrap/viewholders/TileViewHolder.java
+++ app/src/main/java/com/tunjid/androidbootstrap/viewholders/TileViewHolder.java
@@ -13,31 +13,39 @@ import com.tunjid.androidbootstrap.recyclerview.InteractiveViewHolder;
 
 public class TileViewHolder extends InteractiveViewHolder<TileAdapter.AdapterListener> {
 
+    private static final int COLOR_CHANGE_DURATION = 1000;
+    private static final int START_DELAY = 300;
+
     private TextView text;
     private Tile tile;
+
     private final ValueAnimator animator;
+    private final ValueAnimator.AnimatorUpdateListener listener;
 
     public TileViewHolder(View itemView, TileAdapter.AdapterListener scanAdapterListener) {
         super(itemView, scanAdapterListener);
 
         text = itemView.findViewById(R.id.tile_text);
-        animator = ValueAnimator.ofObject(new ArgbEvaluator(),Color.RED);
-        animator.setDuration(1000);
+        animator = ValueAnimator.ofObject(new ArgbEvaluator(), Color.RED);
+        animator.setDuration(COLOR_CHANGE_DURATION);
 
+        listener = this::updateTextColor;
         itemView.setOnClickListener(view -> adapterListener.onTileClicked(tile));
     }
 
     public void bind(Tile tile) {
         this.tile = tile;
         text.setText(tile.getId());
+
         animator.setIntValues(text.getCurrentTextColor(), tile.getColor());
-        animator.addUpdateListener(this::updateTextColor);
+        animator.addUpdateListener(listener);
+        animator.setStartDelay(START_DELAY); // Cheeky bit of code to keep scrolling smooth on fling
         animator.start();
     }
 
     public void unBind() {
         animator.cancel();
-        animator.removeAllUpdateListeners();
+        animator.removeUpdateListener(listener);
     }
 
     private void updateTextColor(ValueAnimator animation) {
