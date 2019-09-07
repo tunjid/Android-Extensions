e7866e78777d3ea0a11fa0b1bb84d450f578f90e
Adetunji Dahunsi <tjdah100@gmail.com>
2019-09-06 20:14:45 -0400

    Moved ViewHider to Dynamic animations

--- view/src/main/java/com/tunjid/androidbootstrap/view/animator/ViewHider.kt
+++ view/src/main/java/com/tunjid/androidbootstrap/view/animator/ViewHider.kt
@@ -6,12 +6,10 @@ import android.graphics.Point
 import android.view.View
 import android.view.WindowManager
 import androidx.annotation.IntDef
-import androidx.core.view.ViewCompat
-import androidx.core.view.ViewPropertyAnimatorListener
 import androidx.core.view.doOnPreDraw
-import androidx.dynamicanimation.animation.SpringForce
-import androidx.dynamicanimation.animation.springAnimationOf
-import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import com.tunjid.androidbootstrap.view.util.spring
 
 /**
  * Translates a view offscreen, useful for adding the quick return pattern to a view.
@@ -24,7 +22,7 @@ class ViewHider private constructor(
         private val listener: Listener,
         @param:HideDirection @field:HideDirection
         private val direction: Int,
-        private val duration: Long) {
+        private val options: SpringAnimation.() -> Unit) {
 
     private var isVisible = true
 
@@ -42,27 +40,22 @@ class ViewHider private constructor(
     private fun toggle(visible: Boolean) {
         if (this.isVisible == visible) return
 
-//        springAnimationOf(::setDropperX, ::getDropperX, point.x).apply {
-//            spring.stiffness = SpringForce.STIFFNESS_MEDIUM
-//            spring.dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
-//            start()
-//        }
-
         // View hasn't been laid out yet and has it's observer attached
         if (!view.isLaidOut) return view.doOnPreDraw { toggle(visible) }
 
         this.isVisible = visible
         val displacement = (if (visible) 0 else getDistanceOffscreen()).toFloat()
 
-        val animator = ViewCompat.animate(view)
-                .setDuration(duration)
-                .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
-                .setListener(listener)
-
-        if (direction == LEFT || direction == RIGHT) animator.translationX(displacement)
-        else animator.translationY(displacement)
-
-        animator.start()
+        view.spring(
+                if (direction == LEFT || direction == RIGHT) SpringAnimation.TRANSLATION_X
+                else SpringAnimation.TRANSLATION_Y
+        )
+                .apply {
+                    options.invoke(this)
+                    for (runnable in listener.startRunnables) runnable.invoke()
+                }
+                .withOneShotEndListener { for (runnable in listener.endRunnables) runnable.invoke() }
+                .animateToFinalPosition(displacement)
     }
 
     // These calculations don't take the status bar into account, unlikely to matter however
@@ -89,42 +82,38 @@ class ViewHider private constructor(
 
         @HideDirection
         private var direction = BOTTOM
-        private var duration = 200L
+        private var options: SpringAnimation.() -> Unit = {}
         private val listener = Listener()
 
         fun setDirection(@HideDirection direction: Int): Builder = apply { this.direction = direction }
 
-        fun setDuration(duration: Long): Builder = apply { this.duration = duration }
+        fun addOptions(options: SpringAnimation.() -> Unit): Builder = apply { this.options = options }
 
         fun addStartRunnable(runnable: () -> Unit): Builder = apply { listener.startRunnables.add(runnable) }
 
         fun addEndRunnable(runnable: () -> Unit): Builder = apply { listener.endRunnables.add(runnable) }
 
-        fun build(): ViewHider = ViewHider(view, listener, direction, duration).apply {
+        fun build(): ViewHider = ViewHider(view, listener, direction, options).apply {
             listener.startRunnables.add(0, startRunnable)
             listener.endRunnables.add(0, endRunnable)
         }
     }
 
-    private class Listener internal constructor() : ViewPropertyAnimatorListener {
-
+    private class Listener internal constructor() {
         val startRunnables = mutableListOf<() -> Unit>()
         val endRunnables = mutableListOf<() -> Unit>()
+    }
 
-        override fun onAnimationStart(view: View) {
-            if (startRunnables.isNotEmpty()) for (runnable in startRunnables) runnable.invoke()
-        }
-
-        override fun onAnimationEnd(view: View) {
-            if (endRunnables.isNotEmpty()) for (runnable in endRunnables) runnable.invoke()
-        }
-
-        override fun onAnimationCancel(view: View) {}
+    private fun SpringAnimation.withOneShotEndListener(onEnd: (canceled: Boolean) -> Unit) = apply {
+        addEndListener(object : DynamicAnimation.OnAnimationEndListener {
+            override fun onAnimationEnd(animation: DynamicAnimation<out DynamicAnimation<*>>?, canceled: Boolean, value: Float, velocity: Float) {
+                removeEndListener(this)
+                onEnd(canceled)
+            }
+        })
     }
 
     companion object {
-
-        private val FAST_OUT_SLOW_IN_INTERPOLATOR = FastOutSlowInInterpolator()
 
         const val LEFT = 0
         const val TOP = 1
