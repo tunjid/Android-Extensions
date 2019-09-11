1cedc53463c77008913f548063826b54fc756e9c
Adetunji Dahunsi <tjdah100@gmail.com>
2019-09-11 10:03:42 -0400

    Moved DoggoRank to RecyclerView Tab

--- app/src/main/java/com/tunjid/androidbootstrap/viewmodels/RouteViewModel.kt
+++ app/src/main/java/com/tunjid/androidbootstrap/viewmodels/RouteViewModel.kt
@@ -14,11 +14,11 @@ class RouteViewModel(application: Application) : AndroidViewModel(application) {
     private val mapping = mutableMapOf(
             R.id.menu_core to listOf(
                     Route(DoggoListFragment::class.java.simpleName, formatRoute(R.string.route_doggo_list)),
-                    Route(DoggoRankFragment::class.java.simpleName, formatRoute(R.string.route_doggo_rank)),
                     Route(HidingViewFragment::class.java.simpleName, formatRoute(R.string.route_hiding_view)),
                     Route(SpanbuilderFragment::class.java.simpleName, formatRoute(R.string.route_span_builder))
             ),
             R.id.menu_recyclerview to listOf(
+                    Route(DoggoRankFragment::class.java.simpleName, formatRoute(R.string.route_doggo_rank)),
                     Route(ShiftingTileFragment::class.java.simpleName, formatRoute(R.string.route_shifting_tile)),
                     Route(EndlessTileFragment::class.java.simpleName, formatRoute(R.string.route_endless_tile))
             ),
@@ -31,7 +31,7 @@ class RouteViewModel(application: Application) : AndroidViewModel(application) {
     fun getRoutes(@IdRes id: Int): List<Route> = mapping[id] ?: mapping.values.flatten()
 
     private fun formatRoute(@StringRes stringRes: Int): CharSequence = getApplication<Application>().run {
-        SpanBuilder.of(this.getString(stringRes))
+        SpanBuilder.of(getString(stringRes))
                 .italic()
                 .underline()
                 .color(this, R.color.colorPrimary)
