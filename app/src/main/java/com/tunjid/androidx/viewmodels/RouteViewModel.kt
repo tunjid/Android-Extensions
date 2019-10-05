e4b7f68de0f55cae3849679dd644e5935932305f
Adetunji Dahunsi <tjdah100@gmail.com>
2019-10-05 14:50:11 -0400

    Split core tab to nav and misc

--- app/src/main/java/com/tunjid/androidx/viewmodels/RouteViewModel.kt
+++ app/src/main/java/com/tunjid/androidx/viewmodels/RouteViewModel.kt
@@ -12,12 +12,10 @@ import com.tunjid.androidx.model.Route
 class RouteViewModel(application: Application) : AndroidViewModel(application) {
 
     private val mapping = mutableMapOf(
-            R.id.menu_core to listOf(
+            R.id.menu_navigation to listOf(
                     Route(DoggoListFragment::class.java.simpleName, formatRoute(R.string.route_doggo_list)),
                     Route(IndependentStackFragment::class.java.simpleName, formatRoute(R.string.route_independent_stack)),
-                    Route(MultipleStackFragment::class.java.simpleName, formatRoute(R.string.route_multiple_inner_stack)),
-                    Route(HidingViewFragment::class.java.simpleName, formatRoute(R.string.route_hiding_view)),
-                    Route(SpanbuilderFragment::class.java.simpleName, formatRoute(R.string.route_span_builder))
+                    Route(MultipleStackFragment::class.java.simpleName, formatRoute(R.string.route_multiple_inner_stack))
             ),
             R.id.menu_recyclerview to listOf(
                     Route(DoggoRankFragment::class.java.simpleName, formatRoute(R.string.route_doggo_rank)),
@@ -27,6 +25,10 @@ class RouteViewModel(application: Application) : AndroidViewModel(application) {
             R.id.menu_communications to listOf(
                     Route(BleScanFragment::class.java.simpleName, formatRoute(R.string.route_ble_scan)),
                     Route(NsdScanFragment::class.java.simpleName, formatRoute(R.string.route_nsd_scan))
+            ),
+            R.id.menu_misc to listOf(
+                    Route(HidingViewFragment::class.java.simpleName, formatRoute(R.string.route_hiding_view)),
+                    Route(SpanbuilderFragment::class.java.simpleName, formatRoute(R.string.route_span_builder))
             )
     )
 
