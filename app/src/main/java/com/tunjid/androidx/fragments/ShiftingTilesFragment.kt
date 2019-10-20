c633fa87ecbb57921aaa877879e1c6b0bca73f22
Adetunji Dahunsi <tjdah100@gmail.com>
2019-10-20 13:15:04 -0400

    Renamed fragments and other route listing texts

--- app/src/main/java/com/tunjid/androidx/fragments/ShiftingTilesFragment.kt
+++ app/src/main/java/com/tunjid/androidx/fragments/ShiftingTilesFragment.kt
@@ -5,29 +5,37 @@ import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.RecyclerView
 import com.tunjid.androidx.PlaceHolder
 import com.tunjid.androidx.R
 import com.tunjid.androidx.adapters.TileAdapter
+import com.tunjid.androidx.adapters.withPaddedAdapter
 import com.tunjid.androidx.baseclasses.AppBaseFragment
 import com.tunjid.androidx.recyclerview.ListManager
 import com.tunjid.androidx.recyclerview.ListManagerBuilder
+import com.tunjid.androidx.uidrivers.SlideInItemAnimator
 import com.tunjid.androidx.view.util.InsetFlags
 import com.tunjid.androidx.view.util.InsetFlags.Companion.NO_BOTTOM
 import com.tunjid.androidx.viewholders.TileViewHolder
-import com.tunjid.androidx.viewmodels.EndlessTileViewModel
-import com.tunjid.androidx.viewmodels.EndlessTileViewModel.Companion.NUM_TILES
-import kotlin.math.abs
+import com.tunjid.androidx.viewmodels.ShiftingTileViewModel
 
-class EndlessTileFragment : AppBaseFragment(R.layout.fragment_route) {
-
-    private val viewModel by viewModels<EndlessTileViewModel>()
-    private lateinit var listManager: ListManager<TileViewHolder, PlaceHolder.State>
+class ShiftingTilesFragment : AppBaseFragment(R.layout.fragment_route) {
 
     override val insetFlags: InsetFlags = NO_BOTTOM
 
+    private val viewModel by viewModels<ShiftingTileViewModel>()
+
+    private lateinit var listManager: ListManager<TileViewHolder, PlaceHolder.State>
+
+    private val fabIconRes: Int
+        get() = if (viewModel.changes()) R.drawable.ic_grid_24dp else R.drawable.ic_blur_24dp
+
+    private val fabText: CharSequence
+        get() = getString(if (viewModel.changes()) R.string.static_tiles else R.string.dynamic_tiles)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.moreTiles.observe(this) { listManager.onDiff(it) }
+        viewModel.watchTiles().observe(this) { listManager.onDiff(it) }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -38,25 +46,26 @@ class EndlessTileFragment : AppBaseFragment(R.layout.fragment_route) {
                 toolbarShows = true,
                 toolBarMenu = 0,
                 fabShows = true,
-                fabIcon = R.drawable.ic_info_outline_24dp,
-                fabText = getString(R.string.tile_info),
                 showsBottomNav = false,
+                fabIcon = fabIconRes,
+                fabText = fabText,
                 navBarColor = ContextCompat.getColor(requireContext(), R.color.white_75),
                 fabClickListener = View.OnClickListener {
-                    uiState = uiState.copy(snackbarText = "There are ${viewModel.tiles.size} tiles")
+                    viewModel.toggleChanges()
+                    uiState = uiState.copy(fabIcon = fabIconRes, fabText = fabText)
                 }
         )
 
         listManager = ListManagerBuilder<TileViewHolder, PlaceHolder.State>()
-                .withRecyclerView(view.findViewById(R.id.recycler_view))
-                .withGridLayoutManager(3)
-                .withAdapter(TileAdapter(viewModel.tiles) { uiState = uiState.copy(snackbarText = it.toString()) })
-                .withEndlessScrollCallback(NUM_TILES) { viewModel.fetchMore() }
-                .addScrollListener { _, dy -> if (abs(dy) > 3) uiState = uiState.copy(fabShows = dy < 0) }
+                .withRecyclerView(view.findViewById<RecyclerView>(R.id.recycler_view)
+                        .apply { itemAnimator = SlideInItemAnimator() }
+                )
+                .withGridLayoutManager(4)
+                .withPaddedAdapter(TileAdapter(viewModel.tiles) { uiState = uiState.copy(snackbarText = it.diffId) }, 4)
                 .build()
     }
 
     companion object {
-        fun newInstance(): EndlessTileFragment = EndlessTileFragment().apply { arguments = Bundle() }
+        fun newInstance(): ShiftingTilesFragment = ShiftingTilesFragment().apply { arguments = Bundle() }
     }
 }
