406ccbc46cf43d64658290b3211e228e8d68e064
Adetunji Dahunsi <tjdah100@gmail.com>
2020-04-25 12:56:16 -0400

    Code clean up

--- app/src/main/java/com/tunjid/androidx/fragments/HidingViewsFragment.kt
+++ app/src/main/java/com/tunjid/androidx/fragments/HidingViewsFragment.kt
@@ -1,17 +1,13 @@
 package com.tunjid.androidx.fragments
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.children
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.ShapeAppearanceModel
-import com.tunjid.androidx.MutedColors
 import com.tunjid.androidx.R
 import com.tunjid.androidx.baseclasses.AppBaseFragment
-import com.tunjid.androidx.core.content.colorAt
 import com.tunjid.androidx.core.content.themeColorAt
-import com.tunjid.androidx.core.graphics.drawable.withTint
 import com.tunjid.androidx.databinding.FragmentHidingViewBinding
 import com.tunjid.androidx.isDarkTheme
 import com.tunjid.androidx.view.animator.ViewHider
@@ -41,18 +37,16 @@ class HidingViewsFragment : AppBaseFragment(R.layout.fragment_hiding_view) {
                 showsBottomNav = false,
                 insetFlags = InsetFlags.ALL,
                 lightStatusBar = !requireContext().isDarkTheme,
-                navBarColor = requireContext().themeColorAt(R.attr.nav_bar_color),
-                backgroundColor = MutedColors.colorAt(requireContext().isDarkTheme, 1)
+                navBarColor = requireContext().themeColorAt(R.attr.nav_bar_color)
         )
 
         val binding = FragmentHidingViewBinding.bind(view)
         val context = view.context
+        val elevation = context.resources.getDimensionPixelSize(R.dimen.sixteenth_margin).toFloat()
 
-        binding.cage.background = MaterialShapeDrawable.createWithElevationOverlay(context).apply {
+        binding.cage.elevation = elevation
+        binding.cage.background = MaterialShapeDrawable.createWithElevationOverlay(context, elevation).apply {
             shapeAppearanceModel = ShapeAppearanceModel().withCornerSize(context.resources.getDimensionPixelSize(R.dimen.half_margin).toFloat())
-            strokeWidth = context.resources.getDimensionPixelSize(R.dimen.eigth_margin).toFloat()
-            strokeColor = ColorStateList.valueOf(context.colorAt(R.color.white))
-            withTint(MutedColors.colorAt(view.context.isDarkTheme, 0))
         }
 
         val leftHider = ViewHider.of(binding.leftButton).setDirection(ViewHider.LEFT).build()
@@ -60,13 +54,15 @@ class HidingViewsFragment : AppBaseFragment(R.layout.fragment_hiding_view) {
         val rightHider = ViewHider.of(binding.rightButton).setDirection(ViewHider.RIGHT).build()
         val bottomHider = ViewHider.of(binding.bottomButton).setDirection(ViewHider.BOTTOM).build()
 
+        val squeeze = context.resources.getDimensionPixelSize(R.dimen.double_margin).toFloat()
+
         binding.shrinkPadding.setOnCheckedChangeListener { _, isChecked ->
             listOf(
                     binding.cage.spring(PaddingProperty.LEFT),
                     binding.cage.spring(PaddingProperty.TOP),
                     binding.cage.spring(PaddingProperty.RIGHT),
                     binding.cage.spring(PaddingProperty.BOTTOM)
-            ).forEach { it.animateToFinalPosition(if (isChecked) 60f else 0f) }
+            ).forEach { it.animateToFinalPosition(if (isChecked) squeeze else 0f) }
         }
 
         binding.shrinkMargin.setOnCheckedChangeListener { _, isChecked ->
@@ -75,7 +71,7 @@ class HidingViewsFragment : AppBaseFragment(R.layout.fragment_hiding_view) {
                     binding.root.spring(MarginProperty.TOP),
                     binding.root.spring(MarginProperty.RIGHT),
                     binding.root.spring(MarginProperty.BOTTOM)
-            ).forEach { it.animateToFinalPosition(if (isChecked) 60f else 0f) }
+            ).forEach { it.animateToFinalPosition(if (isChecked) squeeze else 0f) }
         }
 
         fun onClick(v: View) = when (v.id) {
