cb25e43a6fbf680c937d4667b47c404a108bf9f7
Adetunji Dahunsi <tjdah100@gmail.com>
2020-05-01 08:24:30 -0400

    Added party hard method

--- app/src/main/java/com/tunjid/androidx/fragments/SpringAnimationFragment.kt
+++ app/src/main/java/com/tunjid/androidx/fragments/SpringAnimationFragment.kt
@@ -6,6 +6,7 @@ import android.widget.CheckBox
 import androidx.core.view.children
 import androidx.core.view.postDelayed
 import androidx.dynamicanimation.animation.FloatPropertyCompat
+import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
 import androidx.fragment.app.Fragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -49,16 +50,31 @@ class SpringAnimationFragment : Fragment(R.layout.fragment_spring_animation) {
                 viewHiders.map { it::configure }
                         .plus(marginProperties.toModifiers(view))
                         .plus(paddingProperties.toModifiers(binding.cage))
+                        .plus(viewHiders.map(ViewHider<FloatingActionButton>::view).map(scaleProperties::toModifiers).flatten())
+
+        springModifiers.forEach {
+            it.invoke {
+                stiffness = stiffnessValues.first()
+                dampingRatio = dampingValues.last()
+            }
+        }
 
         uiState = uiState.copy(
                 toolbarTitle = this::class.java.routeName,
-                toolBarMenu = 0,
+                toolBarMenu = R.menu.menu_spring_animations,
                 fabShows = true,
-                fabIcon = R.drawable.ic_expand_24dp,
                 toolbarOverlaps = false,
                 toolbarShows = true,
-                fabText = getString(R.string.spring_options),
-                fabClickListener = { springModifiers.springOptions() },
+                toolbarMenuClickListener = {
+                    springModifiers.springOptions()
+                },
+                fabIcon = R.drawable.ic_dance_24dp,
+                fabText = getString(R.string.party_hard),
+                fabClickListener = {
+                    marginProperties.partyHard(view)
+                    paddingProperties.partyHard(binding.cage)
+                    viewHiders.map(ViewHider<FloatingActionButton>::view).forEach(scaleProperties::partyHard)
+                },
                 showsBottomNav = false,
                 insetFlags = InsetFlags.ALL,
                 lightStatusBar = !requireContext().isDarkTheme,
@@ -120,6 +136,18 @@ private fun List<FloatPropertyCompat<View>>.toModifiers(view: View) =
         map { view.spring(it).spring }
                 .map { { modifier: SpringModifier -> modifier.invoke(it) } }
 
+private fun List<FloatPropertyCompat<View>>.partyHard(view: View) = forEach {
+    val resources = view.context.resources
+    val current = it.getValue(view)
+    val squeeze = resources.getDimensionPixelSize(R.dimen.double_margin).toFloat()
+
+    view.spring(it).animateToFinalPosition(when (it) {
+        SpringAnimation.SCALE_X -> if (current == 1f) 0.8f else 1f
+        SpringAnimation.SCALE_Y -> if (current == 1f) 0.8f else 1f
+        else -> if (current == 0f) squeeze else 0f
+    })
+}
+
 private fun CheckBox.toggleProperty(properties: List<FloatPropertyCompat<View>>, cage: View) {
     val squeeze = context.resources.getDimensionPixelSize(R.dimen.double_margin).toFloat()
     setOnCheckedChangeListener { _, isChecked ->
@@ -145,8 +173,15 @@ private val marginProperties
             MarginProperty.BOTTOM
     )
 
+private val scaleProperties
+    get() = listOf(
+            SpringAnimation.SCALE_X,
+            SpringAnimation.SCALE_Y
+    )
+
 private val Fragment.stiffnessNames
     get() = arrayOf(
+            getString(R.string.bruh),
             getString(R.string.very_low),
             getString(R.string.low),
             getString(R.string.medium),
@@ -158,10 +193,12 @@ private val Fragment.dampingNames
             getString(R.string.none),
             getString(R.string.low),
             getString(R.string.medium),
-            getString(R.string.high)
+            getString(R.string.high),
+            getString(R.string.bruh)
     )
 
 private val stiffnessValues = listOf(
+        10f,
         SpringForce.STIFFNESS_VERY_LOW,
         SpringForce.STIFFNESS_LOW,
         SpringForce.STIFFNESS_MEDIUM,
@@ -172,5 +209,6 @@ private val dampingValues = listOf(
         SpringForce.DAMPING_RATIO_NO_BOUNCY,
         SpringForce.DAMPING_RATIO_LOW_BOUNCY,
         SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY,
-        SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+        SpringForce.DAMPING_RATIO_HIGH_BOUNCY,
+        0.08f
 )
