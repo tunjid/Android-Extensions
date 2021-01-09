7d8d569dad4ff93c66b8e12f25857335f12b0d6f
Adetunji Dahunsi <tjdah100@gmail.com>
2021-01-09 10:51:10 -0500

    Rewrite of suspending fragment transcation ops

--- navigation/src/main/java/com/tunjid/androidx/navigation/CommonSuspendingNavigator.kt
+++ navigation/src/main/java/com/tunjid/androidx/navigation/CommonSuspendingNavigator.kt
@@ -1,12 +1,13 @@
 package com.tunjid.androidx.navigation
 
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.whenResumed
+import kotlin.coroutines.resume
 import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
-import kotlin.coroutines.resume
 
 /**
  * A stateless class that keeps [SuspendingStackNavigator] and [SuspendingMultiStackNavigator] DRY
@@ -20,34 +21,43 @@ internal class CommonSuspendingNavigator(private val navigator: Navigator) : Sus
 
     override suspend fun find(tag: String): Fragment? = navigator.find(tag)
 
-    override suspend fun pop() = mainThreadSuspendCancellableCoroutine<Fragment?> { continuation ->
-        when (val previous = navigator.previous) {
-            null -> continuation.resumeIfActive(null)
+    override suspend fun pop(): Fragment? {
+        val current = navigator.current ?: return null
+        current.whenResumed(doNothing)
+
+        return when (val previous = navigator.previous) {
+            null -> null
             else -> {
-                previous.doOnLifecycleEvent(Lifecycle.Event.ON_RESUME) { continuation.resumeIfActive(previous) }
                 navigator.pop()
+                previous.whenResumed { previous }
             }
         }
     }
 
-    override suspend fun <T : Fragment> push(fragment: T, tag: String) = mainThreadSuspendCancellableCoroutine<T?> { continuation ->
-        when (navigator.current?.tag) {
-            tag -> continuation.resumeIfActive(null)
-            else -> {
-                fragment.doOnLifecycleEvent(Lifecycle.Event.ON_RESUME) { continuation.resumeIfActive(fragment) }
-                navigator.push(fragment, tag)
-            }
+    override suspend fun <T : Fragment> push(fragment: T, tag: String): T? {
+        val current = navigator.current
+
+        if (current == null) {
+            navigator.push(fragment, tag)
+            return fragment.whenResumed { fragment }
         }
+
+        if (current.tag == tag) return null
+
+        current.whenResumed { navigator.push(fragment, tag) }
+        return fragment.whenResumed { fragment }
     }
 
     override suspend fun clear(upToTag: String?, includeMatch: Boolean): Fragment? =
-            throw IllegalArgumentException("Override this")
+        throw IllegalArgumentException("Override this")
 }
 
 internal suspend inline fun <T> mainThreadSuspendCancellableCoroutine(
-        crossinline block: (CancellableContinuation<T>) -> Unit
+    crossinline block: (CancellableContinuation<T>) -> Unit
 ): T = withContext(Dispatchers.Main) { suspendCancellableCoroutine(block) }
 
 internal fun <T> CancellableContinuation<T>.resumeIfActive(item: T) {
     if (isActive) resume(item)
 }
+
+internal val doNothing: suspend CoroutineScope.() -> Unit get() = {}
